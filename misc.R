2b4f53ed650686475a15562f5eb665c40e0b201a
KostasVasilopoulos <k.vasilopoulo@gmail.com>
2019-04-11 18:36:44 +0300

    added logo and changed title

---
+++ misc.R
@@ -0,0 +1,20 @@
+
+library(blogdown)
+
+# Update hugo -------------------------------------------------------------
+
+hugo_version()
+
+# update_hugo()
+
+
+# Update the academic theme -----------------------------------------------
+
+# Do not try it breaks
+# install_theme("gcushen/hugo-academic", force = TRUE)
+
+# Bild site ---------------------------------------------------------------
+
+build_site()
+
+serve_site()
