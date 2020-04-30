fbb56717b86310737c717fe366c4339dd5e189b2
KostasVasilopoulos <k.vasilopoulo@gmail.com>
2020-04-30 18:17:07 +0100

    added blog theme

--- content/cv/.inactive/send.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-mv ~/Documents/website/cv/cv.pdf  ~/Documents/website/static/files
-# mv cv.md  ~/Documents/My_Website/content
--- public/cv/cv/send.R
+++
@@ -1,9 +0,0 @@
-rmarkdown::render("cv/cv.Rmd", encoding = "UTF-8")
-
-file.copy(
-  from =  "cv/cv.pdf",
-  to  = "static/files",
-  recursive = TRUE
-)
-
-if (file.exists(fn <- "cv/cv.tex")) file.remove(fn)
